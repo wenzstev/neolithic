791222118bd2df6cbd2f2dbb7c9f81496b75109d
Steve <wenzelstev@gmail.com>
2025-02-12 23:08:58 -0500

    feat: generalize astar function

---
+++ internal/astar/priorityqueue.go
@@ -0,0 +1,31 @@
+package astar
+
+// PriorityQueue is a heap used to select the searchNode with the lowest cost
+type PriorityQueue []*searchNode
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].fCost() < pq[j].fCost()
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	node := x.(*searchNode)
+	node.index = len(*pq)
+	*pq = append(*pq, x.(*searchNode))
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	node := old[n-1]
+	node.index = -1
+	*pq = old[0 : n-1]
+	return node
+}
